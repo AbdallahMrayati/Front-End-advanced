37e7b4a6042c3071d33a0444a086a15e2c5d6d22
abdullah <abdullah.myt412@gmail.com>
2023-03-23 17:24:56 +0200

    Add bootstrapVue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Test msg="Welcome to Your Vue.js App" />
+    <TestGit msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import Test from "./components/Test.vue";
+import TestGit from "./components/TestGit.vue";
+
 export default {
   name: "App",
   components: {
     HelloWorld,
-    Test,
+    TestGit,
   },
 };
 </script>
