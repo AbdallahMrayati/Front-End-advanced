46c6ec828cfcd920b8057016f341e78ea71fe087
abdullah <abdullah.myt412@gmail.com>
2023-03-23 15:18:48 +0200

    add test file

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Test msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import Test from "./components/Test.vue";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Test,
   },
 };
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <H1>HI i'm a test</H1>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    msg: String,
+  },
+};
+</script>
