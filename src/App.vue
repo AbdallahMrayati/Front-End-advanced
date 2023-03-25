2d1b7139f670cfbe0ada6d82aa048bb6074011c3
abdullah <abdullah.myt412@gmail.com>
2023-03-25 22:18:41 +0200

    fetchAPI

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
+    <div>
+      <b-card
+        :title="openedItem.title"
+        :img-src="openedItem.image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="mb-2 m-5 p-5"
+      >
+        <b-card-text>
+          {{ openedItem.desc }}
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <TestGit msg="Welcome to Your Vue.js App" />
+    <CardsComp :data="cards" @openItem="openTheItem"></CardsComp>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import TestGit from "./components/TestGit.vue";
+import CardsComp from "./components/CardsComp.vue";
 
 export default {
-  name: "App",
+  created() {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((res) => (this.cards = res.products));
+  },
+  data() {
+    return {
+      cards: [],
+      openedItem: [],
+    };
+  },
   components: {
     HelloWorld,
-    TestGit,
+    CardsComp,
+  },
+  methods: {
+    openTheItem(child) {
+      this.openedItem.id = child.id;
+      this.openedItem.title = child.title;
+      this.openedItem.image = child.images[0];
+      this.openedItem.desc = child.description;
+      console.log(this.openedItem);
+    },
   },
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-      <b-dropdown-item>First Action</b-dropdown-item>
-      <b-dropdown-item>Second Action</b-dropdown-item>
-      <b-dropdown-item>Third Action</b-dropdown-item>
-      <b-dropdown-divider></b-dropdown-divider>
-      <b-dropdown-item active>Active action</b-dropdown-item>
-      <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-    </b-dropdown>
-  </div>
+  <h1 class="text-center p-3 text-danger">The first cards by Vue js</h1>
 </template>
 
 <script>
